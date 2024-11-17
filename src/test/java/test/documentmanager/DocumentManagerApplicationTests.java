703c8d469e5de70d45ccd7a411e29bfeeff1dcd1
Denis Derkach <derkach.denis@lll.kpi.ua>
2024-11-17 19:41:06 +0200

    finished task

---
+++ src/test/java/test/documentmanager/DocumentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package test.documentmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
